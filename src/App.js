2c69fa242d772d044253a33fe2c91fa438b234c0
Harini2544 <136494932+Harini2544@users.noreply.github.com>
2024-04-30 01:05:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useReducer, useCallback } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 // Counter context
